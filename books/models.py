091fe10c5dbdb1877b9083647a40f4df84c9d5f3
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-14 16:25:42 -0700

    fixed bug in book model attribute

--- books/models.py
+++ books/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.Charfield(max_length=100)
+    title = models.CharField(max_length=100)
     num_pages = models.IntegerField()
     date_published = models.DateField()
 
