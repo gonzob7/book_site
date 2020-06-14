c6319a12ba94daaa18bb86c54fc34bbf83a2ce33
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-14 16:24:14 -0700

    added return title method

--- books/models.py
+++ books/models.py
@@ -8,3 +8,6 @@ class Book(models.Model):
     title = models.Charfield(max_length=100)
     num_pages = models.IntegerField()
     date_published = models.DateField()
+
+    def __str__(self):
+        return self.title
