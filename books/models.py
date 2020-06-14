47aa0676150b74967b4bb16f363b26b1dcd225bd
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-14 16:41:16 -0700

    added foreignkey to book model

--- books/models.py
+++ books/models.py
@@ -4,17 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    num_pages = models.IntegerField()
-    date_published = models.DateField()
-
-    def __str__(self):
-        return self.title
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    num_pages = models.IntegerField()
+    date_published = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
