94e693efa56ac9aaa7262c7c160281fcca0a7af4
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-14 17:19:30 -0700

    added new tag model with manytomany relationship

--- books/models.py
+++ books/models.py
@@ -11,11 +11,19 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     num_pages = models.IntegerField()
     date_published = models.DateField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
